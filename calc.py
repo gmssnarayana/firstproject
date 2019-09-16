583f7af25a7f43fa7abb3f840832558448c2eaa1
gmss <mgonnaba@in.ibm.com>
2019-09-16 07:05:57 +0000

    calc.py

---
+++ calc.py
@@ -0,0 +1,12 @@
+def add(x, y):
+        ***Add Function***
+            return x + y
+        
+        def subtract(x, y):
+               ***Subtract Funciton***
+                  return x-y
+
+             
+             def mutiply(x, y):
+               ***Multiply Funciton***
+                  return x * y 
